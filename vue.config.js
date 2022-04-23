4418ff3bb87f47582e7e0ca0eee0a78371e2585b
Misha Novikov <novikov1807@gmail.com>
2022-04-23 22:11:50 +0300

    v.1.2

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   publicPath: '/vue2104/'
 };
+https://github.com/NovikovM18/
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue2104/'
+    : '/'
+}
